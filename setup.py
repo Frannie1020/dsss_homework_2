1cd46b9a42a3af390ace6fa7003e2b4c58264280
Frannie1020 <franylinn@gmail.com>
2024-11-08 01:08:48 +0100

    Modify setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,10 @@ setup(
     name='math_quiz',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'numpy==1.21.0',
-        'pandas==1.3.0',
-        'pytest==6.2.4'
-    ],
+    install_requires=[],  # List any dependencies here
     entry_points={
         'console_scripts': [
-            'math_quiz=math_quiz.math_quiz:math_quiz',  # Ensure the `math_quiz.py` file has a `math_quiz()` function
+            'math_quiz=math_quiz.math_quiz:main',  # Ensure that `math_quiz.py` contains a `main()` function
         ],
     },
 )
